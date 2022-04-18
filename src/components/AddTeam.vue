628d576837e6f4cd18f2252bce2864332b1c07e7
Dave Hoy <david.hoy@techelevator.com>
2022-04-18 19:10:07 -0400

    Started work on Add Team dialog

---
+++ src/components/AddTeam.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="addTeamDialog" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs}">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          color="green"
+          v-bind="attrs"
+          v-on="on"
+          >
+            <v-icon dark>
+              mdi-plus
+            </v-icon>
+        </v-btn>
+      </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Add Team</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-text-field label="Team name" v-model.trim="teamName" required/>
+            </v-col>
+            <v-col cols="12" sm="6" md="4">
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="blue darken-1" text @click="addTeam">Add</v-btn>
+      </v-card-actions>
+    </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'add-team',
+  data() {
+    return {
+      addTeamDialog: false,
+      teamName: ""
+    }
+  },
+  methods: {
+    addTeam() {
+      alert(`Adding team ${this.teamName}`);
+      this.teamName = '';
+      this.addTeamDialog = false;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/components/TeamDisplay.vue
+++ src/components/TeamDisplay.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container>
-    <v-tabs v-model="model" centered>
-      <v-tab
-        v-for="team in $store.state.teams"
-        :key="team.id"
-        :href="`#tab-${team.id}`"
-      >
-        {{ team.name }}
-      </v-tab>
-    </v-tabs>
+      <v-tabs v-model="model" centered height="60px">
+        <v-tab
+          v-for="team in $store.state.teams"
+          :key="team.id"
+          :href="`#tab-${team.id}`"
+        >
+          {{ team.name }}
+        </v-tab>
+        <v-spacer/>
+        <add-team/>
+      </v-tabs>
     <v-tabs-items v-model="model">
       <v-tab-item
         v-for="team in $store.state.teams"
@@ -28,17 +30,30 @@
         </v-card>
       </v-tab-item>
     </v-tabs-items>
+    
+
+
   </v-container>
 </template>
 
 <script>
+import AddTeam from './AddTeam.vue';
 export default {
   name: "team-display",
+  components: { 
+    AddTeam 
+  },
   data() {
     return {
       model: {},
+      addTeamDialog: false
     };
   },
+  methods: {
+    addTeam() {
+      alert('Adding a new team');
+    }
+  }
 };
 </script>
 
